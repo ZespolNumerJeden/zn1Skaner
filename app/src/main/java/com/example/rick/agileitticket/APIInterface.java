826ef606c6b6d64334782ac5fa53a46cbbabc4c2
Rick <orzech@desmart.com>
2017-11-20 13:36:59 +0100

    crash fix

--- app/src/main/java/com/example/rick/agileitticket/APIInterface.java
+++ app/src/main/java/com/example/rick/agileitticket/APIInterface.java
@@ -7,6 +7,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.PATCH;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 interface APIInterface {
@@ -21,5 +22,7 @@ interface APIInterface {
 
     @PATCH("/api/ticket/{ticket}")
     Call<ApiResponse> setWasInPast(@Path("ticket") String ticketId, @Header("WasInPast") boolean was_in_past);
+
+    
 
 }
